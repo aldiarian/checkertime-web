dfa91a90ff9d3ef4bc7220f615c7d348a40087fc
Alberto <alberto@elojografico.net>
2017-04-16 21:49:51 +0200

    estilos

--- js/slidetounlock.js
+++ js/slidetounlock.js
@@ -1,33 +1,40 @@
 $(function() {
 
-	var tamano = $("#fichar_entrada").width();
-	console.log('tamaño del slider: ' + tamano );
-	var pararen = tamano - 80;
-	var parar = true;
-	
-	$('#slider_entrada')[0].addEventListener('touchmove', function(event) {
-	    event.preventDefault();
-	    var el = event.target;
-	    var touch = event.touches[0];
-	    curX = touch.pageX - this.offsetLeft - 73;
-	    if(curX <= 0) return;
-	    if(curX > pararen){
-	    	el.style.webkitTransform = 'translateX(' + pararen + 'px)'; 
-	    	parar=false;
-	    } else{
-	   		el.style.webkitTransform = 'translateX(' + curX + 'px)'; 
-	    }
+	$('.slider').on('touchmove click', function(e){
 
-	
-	}, false);
-	
-	$('#slider_entrada')[0].addEventListener('touchend', function(event) {	
-	    this.style.webkitTransition = '-webkit-transform 0.3s ease-in';
-	    this.addEventListener( 'webkitTransitionEnd', function( event ) { this.style.webkitTransition = 'none'; }, false );
-	    if(parar){
-		    this.style.webkitTransform = 'translateX(0px)';
-		    this.addClass('cambiar');
-	    }
-	}, false);
+			var elemento = $(this);
+			var tamanoPadre = elemento.parent().width();
+			var padre = tamanoPadre - 80;
+			var parar = false;
+
+					elemento.on('touchmove click',  function(event) {
+						var estafichado = elemento.parent().hasClass('fichado');
+						if(!estafichado){
+						    event.preventDefault();
+						    var el = event.target;
+						    var touch = event.touches[0];
+						    curX = touch.pageX - this.offsetLeft - 73;
+						    if(curX <= 0) return;
+						    if(curX > padre){
+						    	el.style.webkitTransform = 'translateX(' + padre + 'px)'; 
+						    	parar = true;
+						    	elemento.parent().toggleClass('fichado');
+						    } else{
+						   		el.style.webkitTransform = 'translateX(' + curX + 'px)'; 
+						    }
+						}
+
+					});
+					
+					elemento.on('touchend click', function (){
+						this.style.webkitTransition = '-webkit-transform 0.1s ease-in';
+						this.addEventListener( 'webkitTransitionEnd', function( event ) { this.style.webkitTransition = 'none'; }, false );
+						if(!elemento.parent().hasClass('fichado')){
+							this.style.webkitTransform = 'translateX(0px)';
+						}
+					});
+
+
+	});
 
 });
