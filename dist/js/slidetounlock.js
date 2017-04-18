26471e12f6a3b3b05863487bd236793fe28d8497
Alberto <alberto@elojografico.net>
2017-04-18 13:05:21 +0200

    stilos

---
+++ dist/js/slidetounlock.js
@@ -0,0 +1,40 @@
+$(function() {
+
+	$('.slider').on('touchmove click', function(e){
+
+			var elemento = $(this);
+			var tamanoPadre = elemento.parent().width();
+			var padre = tamanoPadre - 80;
+			var parar = false;
+
+					elemento.on('touchmove click',  function(event) {
+						var estafichado = elemento.parent().hasClass('fichado');
+						if(!estafichado){
+						    event.preventDefault();
+						    var el = event.target;
+						    var touch = event.touches[0];
+						    curX = touch.pageX - this.offsetLeft - 73;
+						    if(curX <= 0) return;
+						    if(curX > padre){
+						    	el.style.webkitTransform = 'translateX(' + padre + 'px)'; 
+						    	parar = true;
+						    	elemento.parent().toggleClass('fichado');
+						    } else{
+						   		el.style.webkitTransform = 'translateX(' + curX + 'px)'; 
+						    }
+						}
+
+					});
+					
+					elemento.on('touchend click', function (){
+						this.style.webkitTransition = '-webkit-transform 0.1s ease-in';
+						this.addEventListener( 'webkitTransitionEnd', function( event ) { this.style.webkitTransition = 'none'; }, false );
+						if(!elemento.parent().hasClass('fichado')){
+							this.style.webkitTransform = 'translateX(0px)';
+						}
+					});
+
+
+	});
+
+});
--- gulpfile.js
+++ gulpfile.js
@@ -2,20 +2,47 @@ var gulp        = require('gulp');
 var browserSync = require('browser-sync').create();
 var sass        = require('gulp-sass');
 const autoprefixer = require('gulp-autoprefixer');
+var fileinclude = require('gulp-file-include');
+
+var addsrc = require("gulp-add-src");
 
 
 // Static Server + watching scss/html files
-gulp.task('serve', ['sass' ], function() {
+gulp.task('serve', ['sass' , 'fileinclude', 'copytodist' ],  function() {
 
     browserSync.init({
-        server: "./"
+        server: "./dist/"
     });
 
     gulp.watch("scss/**/*.scss", ['sass']);
+    gulp.watch("*.html", ['fileinclude']);
     gulp.watch("*.html").on('change', browserSync.reload);
+    gulp.watch("modulos/*.html").on('change', browserSync.reload);
     gulp.watch("js/**/*.js").on('change', browserSync.reload);
 
+   
 });
+
+gulp.task('copytodist', function(){
+    gulp.src('./')
+        .pipe(addsrc('img/**', { base: '.'}))
+        .pipe(addsrc('fonts/**', { base: '.'}))
+        .pipe(addsrc('js/**', { base: '.'}))
+        .pipe(gulp.dest('dist'));
+
+});
+
+
+     
+gulp.task('fileinclude', function() {
+  gulp.src(['**/*.html'])
+    .pipe(fileinclude({
+      prefix: '@@',
+      basepath: 'modulos'
+    }))
+    .pipe(gulp.dest('dist'));
+});
+
 
 
 // Compile sass into CSS & auto-inject into browsers
@@ -26,7 +53,7 @@ gulp.task('sass', function() {
             browsers: ['last 2 versions'],
             cascade: false
         }))
-        .pipe(gulp.dest("css"))
+        .pipe(gulp.dest("dist/css"))
         .pipe(browserSync.stream());
 });
 
